59e8c6e88bef751d2e7ecabd8bf93ddfc3ee825d
Sergey Prokopchuk <svp@anahoret.com>
2016-09-19 13:21:17 +0300

    DIVIDE & CONQUER ALGORITHM - Calculate inversions in array

--- spec/task1_spec.rb
+++ spec/task1_spec.rb
@@ -5,7 +5,7 @@ describe 'task 1' do
   let(:output_arrs) {[[1, 2, 3, 3, 6, 7, 9, 12, 15, 19], [3, 4, 6, 8, 9, 10, 10, 25, 29], [-5, 4, 6, 6, 8, 8, 9, 10], [1, 3, 6, 7, 15], [1, 3, 6, 7, 15]] }
 
   describe '#merge' do
-    left_arrs = [[1, 3, 6, 7, 15], [6, 8, 10, 25], [4, 6, 8, 10], [1, 3, 6], [7, 15]]
+    left_arrs = [[1, 3, 6, 7, 15], [6, 8, 10, 25], [4, 6, 8, 10], [13, 6], [7, 15]]
     rigth_arrs = [[2, 3, 9, 12, 19], [3, 4, 9, 10, 29], [-5, 6, 8, 9], [7, 15], [1, 3, 6]]
 
     left_arrs.each_with_index do |e, i|
@@ -20,6 +20,15 @@ describe 'task 1' do
     arrs.each_with_index do |value, index|
       it 'returns sorted array' do
         expect(merge_sort(value)).to eq(output_arrs[index])
+      end
+    end
+  end
+
+  describe '#calc_inversions' do
+    arrs = { 3 => [1, 3, 5, 2, 4, 6], 5 => [2, 3, 8, 6, 1], 1 => [2, 1] }
+    arrs.each do |n, arr|
+      it 'returns number of inversions' do
+        expect(calc_inversions(arr).first).to eq n
       end
     end
   end
--- task1.rb
+++ task1.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 def merge left_arr, right_arr
   result = []
   i = j = 0
@@ -23,3 +25,48 @@ def merge_sort arr
   right_arr = merge_sort arr[half, n]
   merge left_arr, right_arr
 end
+
+
+# Inversion
+#i < j
+#A[i] > A[j]
+# left inversion if (i, j) =< N/2
+# right inversion if (i, j) > N/2
+# split inversion if i =< N/2 < j
+
+def calc_inversions arr
+  n = arr.length
+  return [0, arr] if n  == 1
+  half = n.even? ? n / 2 : (n + 1) / 2
+  left_inversions, left_arr = calc_inversions(arr[0, half])
+  right_inversions, right_arr = calc_inversions(arr[half, n])
+  num_inversions, sorted_arr = count_and_sort_split_inversions(left_arr, right_arr)
+  num_inversions = num_inversions + left_inversions + right_inversions
+  [num_inversions, sorted_arr]
+end
+
+def count_and_sort_split_inversions left_arr, right_arr, num_inversions = 0
+  result = []
+  i = j = 0
+  while left_arr.length > i && right_arr.size > j
+    if left_arr[i] > right_arr[j]
+      result << right_arr[j]
+      num_inversions += left_arr.length - i
+      j += 1
+    elsif right_arr[j] >= left_arr[i]
+      result << left_arr[i]
+      i += 1
+    end
+  end
+  result += right_arr[j, right_arr.length] if right_arr[j]
+  result += left_arr[i, left_arr.length] if left_arr[i]
+  [num_inversions, result]
+end
+
+def find_inversions
+  arr = File.readlines('integer_array.txt')
+  arr.map!(&:to_i)
+  calc_inversions(arr).first
+end
+
+puts find_inversions
